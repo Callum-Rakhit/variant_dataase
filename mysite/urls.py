a58df24d12d65819cee30eb3dcc7b417882a69d2
unknown <callum_rakhit@hotmail.com>
2020-02-27 10:54:30 +0000

    new example

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,34 +1,9 @@
-"""
-mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
-from ajax_select import urls as ajax_select_urls
-from django.contrib.auth import views as auth_views
 
+admin.autodiscover()
 
-urlpatterns = [
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('vPanelDBapp.urls')),
-    url(r'^ajax_select/', include(ajax_select_urls)),
-    url(r'^chaining/', include('smart_selects.urls')),
-    url(r'^accounts/login/$', view=auth_views.login, name='login'),
-    url(r'^accounts/logout/$', view=auth_views.logout, name='logout'),
-    url(r'^select2/', include('django_select2.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
--- vPanelDBapp/templatetags/addcss.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='has_group')
-def has_group(user, permissiongroup):
-    groups = [item.strip() for item in permissiongroup.split(",")]  # remove whitespace
-    if str(user.permissiongroup) in groups:
-        return True
