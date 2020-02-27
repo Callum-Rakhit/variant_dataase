0e3c70a38287bc603c4f305ca554951b9d10bdfc
unknown <callum_rakhit@hotmail.com>
2020-02-27 16:34:32 +0000

    fixed issue with urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,28 @@
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path, include, re_path
-from django.views.generic.base import RedirectView # Allows us to redirect incorrect urls to the home page
+from django.urls import path
 
-from blog.views.register import RegisterView
+from blog.views.comment import CommentCreate
+from blog.views.home import home
+from blog.views.post import PostView, PostCreate, PostUpdate, PostDelete
+from blog.views.variant import VariantView, VariantCreate, VariantUpdate, VariantDelete
 
+app_name = 'blog'
 urlpatterns = [
-    path('home/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home:home')),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('', include('django.contrib.auth.urls')),
-    re_path(r'^.*$', RedirectView.as_view(url='home/', permanent=False), name='index') # Django now uses re-path instead of regex on url. This returns incorrect urls to home.
+    # ex: /blog/
+    path('', home, name='home'),
+    # ex: /blog/dusan
+    path('<str:username>', home, name='user_posts'),
+    # ex: /blog/post/5/
+    path('post/<int:pk>/', PostView.as_view(), name='post'),
+    # ex: /blog/post/create/
+    path('post/create/', PostCreate.as_view(), name='create_post'),
+    # ex: /blog/post/5/update/
+    path('post/create/<int:pk>/update', PostUpdate.as_view(), name='update_post'),
+    # ex: /blog/post/5/delete/
+    path('post/<int:pk>/delete/', PostDelete.as_view(), name='delete_post'),
+    # ex: /blog/post/5/comment/
+    path('variant/<int:pk>/comment/', CommentCreate.as_view(), name='create_comment'),
+    # add variant urls
+    path('variant/create/', VariantCreate.as_view(), name='create_variant'),
+    path('variant/create/<int:pk>/update', VariantUpdate.as_view(), name='update_variant'),
+    path('variant/<int:pk>/delete/', VariantDelete.as_view(), name='delete_variant')
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic.base import RedirectView # Allows us to redirect incorrect urls to the home page
 
 from blog.views.register import RegisterView
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    path('home/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='blog:home')),
     path('register/', RegisterView.as_view(), name='register'),
-    path('', include('django.contrib.auth.urls'))
-    # url(r'^.*$', RedirectView.as_view(url='blog/', permanent=False), name='index')
+    path('', include('django.contrib.auth.urls')),
+    re_path(r'^.*$', RedirectView.as_view(url='home/', permanent=False), name='index') # Django now uses re-path instead of regex on url. This returns incorrect urls to home.
 ]
